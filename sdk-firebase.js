f2fdc3d3928415c655823c0b8082a2b24a15aa7c
drhcastro <drhcastrovillela@gmail.com>
2025-08-13 18:04:24 -0700

    Create sdk-firebase.js

---
+++ sdk-firebase.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDHddmg0OawLjgLRe0JG60grZz7hmjZjdk",
+    authDomain: "lista-supermercado-moni.firebaseapp.com",
+    projectId: "lista-supermercado-moni",
+    storageBucket: "lista-supermercado-moni.firebasestorage.app",
+    messagingSenderId: "325452730885",
+    appId: "1:325452730885:web:8ab92f84c6ead9280a026c"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
